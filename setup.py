de441fd0407332860b2954c99202558db6e0f858
safnasNED25 <safnas2042ahamed@gmail.com>
2026-03-14 23:41:58 +0530

    setup.py complete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Zomato-chatbot',
+    version= '0.0.1',
+    author= 'sfns',
+    author_email= 'safnas2042ahamed@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
